6a0c0180354db5b85dff738d9db7283c3c846139
krutika <137177323+uygsj@users.noreply.github.com>
2023-08-12 15:19:59 +0530

    Add files via upload

---
+++ ExpenseItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+function ExpenseDetails(props) {
+    return (
+        <div className="expense-details">
+            <p>Title: {props.title}</p>
+            <p>Amount: {props.amount}</p>
+            <p>Location: {props.location}</p>
+        </div>
+    );
+}
+
+function ExpenseItem(props) {
+
+    return (
+        <div className='expense-item'>
+
+        <ExpenseDate date ={props.date} />
+        <h2>ExpenseDetails</h2>
+        
+        <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+        </div>
+    );
+}
+
+export default ExpenseItem;
